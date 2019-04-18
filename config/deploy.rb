b3797949234f537d877bf407c37069396c31d89e
katotakumi <katotaku0084@gmail.com>
2019-04-18 12:51:38 +0900

    deploy.rb 修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 
-lock '3.11.0'
+lock '~> 3.11.0'
 
 set :application, 'chat-space'
 set :repo_url,  'git@github.com:katotakumi/chat-space.git'
